2cc082985aadb7028a3a5939b5669bd2438b91d2
siddharthpaul20 <siddharthpaul18@gmail.com>
2022-01-03 02:47:19 +0530

    Completed Pin component of the share me app

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -8,6 +8,7 @@ import Pins from './Pins';
 import { userQuery } from '../utils/data';
 import { client } from '../client';
 import logo from '../assets/logo.png';
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
 
@@ -15,7 +16,7 @@ const Home = () => {
     const [user, setUser] = useState(null);
     const scrollRef = useRef(null);
 
-    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    const userInfo = fetchUser();
 
     useEffect(() => {
         const query = userQuery(userInfo?.googleId);
